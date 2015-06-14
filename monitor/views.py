65aac39af9635e243906b95489c75fa9bd28d1aa
HSYang <hsinhd71@gmail.com>
2015-06-14 16:24:46 +0900

    0611 before ajax

--- monitor/views.py
+++ monitor/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from .models import OperationLogger, TemperatureLogger, Flo 연결wmeterLogger
 
 def index(request):
-	url = 'monitor/index.html'
-	context = {}
+	latest_operation_data = OperationLogger.objects.latest('date')
+	latest_temperature_data = TemperatureLogger.objects.latest('date')
+	latest_flowmeter_data = FlowmeterLogger.objects.latest('date')
+
+	switched = check_operation()
+	url = 'index.html'
+	context = {
+		"operation_data": latest_operation_data,
+		"temperature_data": latest_temperature_data,
+		"flowmeter_data": latest_flowmeter_data,
+	}
 	return render(request, url, context)
